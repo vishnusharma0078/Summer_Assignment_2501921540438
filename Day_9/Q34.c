12557cd7adb628b221ec316af24a5bebf42a2a6e
Vishnu Sharma <88197254+vishnusharma0078@users.noreply.github.com>
2026-06-15 14:56:32 +0530

    Day_9 problems done

--- Day_9/Q34.c
+++ Day_9/Q34.c
@@ -1,6 +1,17 @@
-// Day 9 - Question 4
 #include <stdio.h>
 
 int main() {
+    int rows, i, j;
+
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+
+    for (i = rows; i >= 1; i--) {
+        for (j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+
     return 0;
 }
