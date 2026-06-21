1c64260bb1b8b30064d364ffc3cc6a13f316bf3e
Vishnu Sharma <88197254+vishnusharma0078@users.noreply.github.com>
2026-06-22 00:51:51 +0530

    Day_18 problems done

--- Day_18/Q70.c
+++ Day_18/Q70.c
@@ -1,6 +1,36 @@
-// Day 18 - Question 2
 #include <stdio.h>
 
 int main() {
+    int arr[100], n, i, j, minIndex, temp;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Selection Sort
+    for (i = 0; i < n - 1; i++) {
+        minIndex = i;
+
+        for (j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        // Swap elements
+        temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
+    }
+
+    printf("Sorted array:\n");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
     return 0;
 }
